289024dd1e3d82bf2cd549e66a14b02f3a506262
Echo Li <eli@lcs-mba3249.lcs.on.ca>
2017-09-30 11:22:28 -0400

    I try my best to made the rainbow...But I still confused.

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -19,10 +19,10 @@
 // These are some required statements to make this playground work.
 import Cocoa
 import PlaygroundSupport
-
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
-
+let canvas = Canvas(width: 500, height: 500)
+canvas.drawRectangle(centreX: 250, centreY: 2050, width: 500, height: 500)
+canvas.drawRectangle(centreX: 250, centreY: 2050, width: 500, height: 500)
 // Draw a circle at the origin with radius of 50 pixels
 canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
 
@@ -30,10 +30,45 @@ canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
 canvas.drawAxes()
 
 // Draw an ellipse in a different color at the centre of the canvas
+
+// Draw a Semi-circular at the middle
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 70, fromY: 500, toX: 375, toY: 500)
+canvas.drawLine(fromX: 70, fromY: 480, toX: 500, toY: 400)
+canvas.drawLine(fromX: 70, fromY: 460, toX: 500, toY: 300)
+canvas.drawLine(fromX: 60, fromY: 440, toX: 500, toY: 200)
+canvas.drawLine(fromX: 20, fromY: 430, toX: 500, toY: 100)
+canvas.drawLine(fromX: 10, fromY: 430, toX: 390, toY: 0)
+canvas.drawLine(fromX: 5, fromY: 430, toX: 100, toY: 0)
+canvas.fillColor = Color.red
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 500, height: 500)
+canvas.fillColor = Color.orange
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 450, height: 450)
+canvas.fillColor = Color.yellow
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 400, height: 400)
 canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 350, height: 350)
+canvas.fillColor = Color.blue
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 300, height: 300)
+canvas.fillColor = Color.purple
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 250, height: 250)
+canvas.fillColor = Color.blue
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 200, height: 200)
+
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 425, centreY: 500, width: 100, height: 100)
+canvas.drawEllipse(centreX: 480, centreY: 470, width: 130, height: 130)
+
+canvas.fillColor = Color.yellow
+canvas.drawEllipse(centreX: 25, centreY: 475, width: 100, height: 100)
+
+canvas.drawLine(fromX: 290, fromY: 90, toX: 380, toY: 0)
 
 //Add a rectangle to the top of the screen
-canvas.drawRectangle(centreX: 150, centreY: 400, width: 100, height: 50)
+
+
+
+
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
+
